cc0a4461648a74dc2d1ee116f195eb0bef2ba7b9
jan hoogenraad <jan-perl@hoogenraad.net>
2023-04-19 13:55:37 +0200

    git used now

--- addl_config.py
+++ addl_config.py
@@ -11,7 +11,10 @@ def post_save(model, os_path, contents_manager):
     d, fname = os.path.split(os_path)
     if fname.endswith('.ipynb'):
         return # dont do this for notebooks source
-    check_call(["ci","-d","-l","-t-title","-m-msg", fname], cwd=d)
+# old RCS style check_call(["ci","-d","-l","-t-title","-m-msg", fname], cwd=d)
+    check_call(["git","add", fname], cwd=d)
+    check_call(["git","commit","-m-msg", fname], cwd=d)
+    check_call(["git","push"], cwd=d)
 
 c.FileContentsManager.post_save_hook = post_save
 
