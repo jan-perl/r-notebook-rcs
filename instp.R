6777b929487f043b58e05c162981161cd39e7464
jan hoogenraad <jan-perl@hoogenraad.net>
2024-10-04 12:57:03 +0200

    install via R instead of conda

---
+++ instp.R
@@ -0,0 +1,12 @@
+#    install.packages("remotes")
+#    remotes::install_version("rlang", "1.1.2")
+#after installing docker container was 0.4.6 / 1.0.0 -> after  install.packages("dplyr",dependencies=TRUE) -> 1.1.2/1.1.4
+    install.packages("dplyr",dependencies=TRUE)
+    install.packages("remotes")
+    remotes::install_version("dplyr", "1.1.4")
+#after installing docker container was 0.4.6 / 1.0.0 -> after  install.packages("dplyr",dependencies=TRUE) -> 1.1.2/1.1.4
+#    install.packages("dplyr",dependencies=TRUE)
+  install.packages("aws.s3")
+  install.packages("openxlsx")
+  install.packages("ggplot2")
+  install.packages("nnls")
